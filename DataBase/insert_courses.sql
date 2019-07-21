7c37911ed14829b5e1b55133b0625a279426882c
Michael Bzms <35953365+michaelbzms@users.noreply.github.com>
2019-07-21 14:10:31 +0300

    sample classes and name fix in schematic

---
+++ DataBase/insert_courses.sql
@@ -0,0 +1,29 @@
+INSERT INTO `CoursesAppDB`.`Courses` (`idCourses`, `title`, `ects`, `path`, `type`, `specificpath`) 
+VALUES (default, 'Εισαγωγή στον Προγραμματισμό', 7, 'core', 'obligatory', NULL);
+
+INSERT INTO `CoursesAppDB`.`Courses` (`idCourses`, `title`, `ects`, `path`, `type`, `specificpath`) 
+VALUES (default, 'Διακριτά Μαθηματικά', 7, 'core', 'obligatory', NULL);
+
+INSERT INTO `CoursesAppDB`.`Courses` (`idCourses`, `title`, `ects`, `path`, `type`, `specificpath`) 
+VALUES (default, 'Λογική Σχεδίαση', 6, 'core', 'obligatory', NULL);
+
+INSERT INTO `CoursesAppDB`.`Courses` (`idCourses`, `title`, `ects`, `path`, `type`, `specificpath`) 
+VALUES (default, 'Γραμμική Άλγεβρα', 6, 'core', 'obligatory', NULL);
+
+INSERT INTO `CoursesAppDB`.`Courses` (`idCourses`, `title`, `ects`, `path`, `type`, `specificpath`) 
+VALUES (default, 'Ανάλυση Ι', 8, 'core', 'obligatory', NULL);
+
+INSERT INTO `CoursesAppDB`.`Courses` (`idCourses`, `title`, `ects`, `path`, `type`, `specificpath`) 
+VALUES (default, 'Ανάλυση ΙΙ', 8, 'core', 'obligatory', NULL);
+
+INSERT INTO `CoursesAppDB`.`Courses` (`idCourses`, `title`, `ects`, `path`, `type`, `specificpath`) 
+VALUES (default, 'Δομές Δεδομένων και Τεχνικές Προγραμματισμού', 7, 'core', 'obligatory', NULL);
+
+INSERT INTO `CoursesAppDB`.`Courses` (`idCourses`, `title`, `ects`, `path`, `type`, `specificpath`) 
+VALUES (default, 'Ηλεκτρομαγνητισμός, Οπτική και Σύγχρονη Φυσική', 8, 'core', 'obligatory', NULL);
+
+INSERT INTO `CoursesAppDB`.`Courses` (`idCourses`, `title`, `ects`, `path`, `type`, `specificpath`) 
+VALUES (default, 'Αρχιτεκτονική Υπολογιστών Ι', 7, 'core', 'obligatory', NULL);
+
+INSERT INTO `CoursesAppDB`.`Courses` (`idCourses`, `title`, `ects`, `path`, `type`, `specificpath`) 
+VALUES (default, 'Αντικειμενοστραφής Προγραμματισμός', 8, 'core', 'obligatory', NULL);
